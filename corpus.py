b37e7f85a9a479d4b77a3246ddd46c3c796f75d6
Naoya Kanai <naopon@gmail.com>
2016-09-13 14:52:17 -0700

    model train

---
+++ corpus.py
@@ -0,0 +1,50 @@
+from music21 import converter, clef, stream, pitch, note, meter, midi
+import numpy as np
+
+
+KEY_SIG_OFFSET = 0
+
+def parse_notes(midi_stream):
+    melody_corpus = []
+
+    last_pitch = 1
+    chord_buffer = []
+    prev_offset = 0.0
+    for m in midi_stream.measures(1, None):
+        time_sig = m.timeSignature
+        for nr in m.flat.notesAndRests:
+            offset_loc = nr.offset
+            pitch = nr.pitch.pitchClass + 1  if isinstance(nr, note.Note) else 0
+            beat_strength = round(nr.beatStrength * 4.0, 0)
+            duration = float(nr.quarterLength)
+
+            note_repr = (pitch, beat_strength, duration)
+            # Handle chords
+            if nr.offset == prev_offset:
+                if note_repr[0] > 0:
+                    chord_buffer.append(note_repr)
+            else:
+                if chord_buffer: # Choose tone from chord buffer closest to current note
+                    chord_melody_tone = sorted(chord_buffer, key=lambda x: abs(x[0] - pitch))[0]
+                    melody_corpus.append(chord_melody_tone)
+                melody_corpus.append(note_repr)
+                chord_buffer = []
+            prev_offset = nr.offset
+
+    return melody_corpus
+
+
+def build_corpus(midi_files):
+    melody_corpus = []
+    for file in midi_files:
+        midi_stream = converter.parse(file)
+        midi_stream = midi_stream[0]
+        key_sig = midi_stream.keySignature
+        midi_stream.transpose(KEY_SIG_OFFSET - key_sig.tonic.pitchClass, inPlace=True)
+        melody_corpus.extend(parse_notes(midi_stream))
+    # map indices for constructing matrix representations
+    melody_set = set(melody_corpus)
+    notes_indices = {note: i for i, note in enumerate(melody_set)}
+    indices_notes = {i: note for i, note in enumerate(melody_set)}
+
+    return melody_corpus, melody_set, notes_indices, indices_notes
---
+++ train.py
@@ -0,0 +1,57 @@
+import numpy as np
+from keras.models import Sequential, load_model
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM
+from keras.callbacks import History, ModelCheckpoint
+
+from corpus import build_corpus
+
+
+
+def train_bach_corpus(save_path, model_path=None, batch_size=128, nb_epoch=1):
+    midi_files = []
+    with open('midi_files') as filelist:
+        for f in filelist:
+            midi_files.append(f.replace('\n', ''))
+
+    melody_corpus, melody_set, notes_indices, indices_notes = build_corpus(midi_files)
+
+    corpus_size = len(melody_set)
+
+    # cut the corpus into semi-redundant sequences of max_len values
+    step_size = 5
+    phrase_len = 50
+    phrases = []
+    next_notes = []
+    for i in range(0, len(melody_corpus) - phrase_len, step_size):
+        phrases.append(melody_corpus[i: i + phrase_len])
+        next_notes.append(melody_corpus[i + phrase_len])
+    print('nb sequences:', len(phrases))
+
+    # transform data into binary matrices
+    X = np.zeros((len(phrases), phrase_len, corpus_size), dtype=np.bool)
+    y = np.zeros((len(phrases), corpus_size), dtype=np.bool)
+    for i, phrase in enumerate(phrases):
+        for j, note in enumerate(phrase):
+            X[i, j, notes_indices[note]] = 1
+        y[i, notes_indices[next_notes[i]]] = 1
+    if model_path is None:
+        model = Sequential()
+        model.add(LSTM(512, return_sequences=True, input_shape=(phrase_len, corpus_size)))
+        model.add(Dropout(0.2))
+        model.add(LSTM(512, return_sequences=False))
+        model.add(Dropout(0.2))
+        model.add(Dense(corpus_size))
+        model.add(Activation('softmax'))
+
+        model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+    else:
+        model = load_model(model_path)
+
+    checkpoint = ModelCheckpoint(filepath=save_path,
+        verbose=1, save_best_only=False)
+    history = History()
+    model.fit(X, y, batch_size=batch_size, nb_epoch=nb_epoch, callbacks=[checkpoint, history])
+
+    return model
